254ba012b2526e0ded20265320036a2001542a2d
Jirka Kremser <jkremser@redhat.com>
2018-12-03 19:16:26 +0100

    removing other unused stuff

---
+++ example-cr.js
@@ -0,0 +1,18 @@
+load("common.js")
+//todo
+
+// on add handler
+var onAdd = function(foo) {
+  print("Hello from JavaScript, added " + foo.name);
+};
+
+// on delete handler
+var onDelete = function(foo) {
+  print("Hello from JavaScript, deleted " + foo.name);
+};
+
+// start the operator
+startOperator(onAdd, onDelete);
+
+// sleep 10 years
+java.lang.Thread.sleep(1000 * 60 * 60 * 24 * 365 * 10);
